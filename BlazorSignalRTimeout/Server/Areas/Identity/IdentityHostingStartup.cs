92c2ba15142c24a0616835bc5272e697966d58bc
Kjell Ahlstrom <kjell.ahlstrom@gmail.com>
2020-09-06 10:31:28 +0200

    initial commit

---
+++ BlazorSignalRTimeout/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BlazorSignalRTimeout.Server.Data;
+using BlazorSignalRTimeout.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorSignalRTimeout.Server.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorSignalRTimeout.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
